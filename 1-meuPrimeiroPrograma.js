a5b028c8a7aa71a07aba7bc503ff5b7185a7a260
Eduardo <xeduxbrux@gmail.com>
2021-06-03 15:22:19 -0300

    Explicação das variáveis

--- 1-meuPrimeiroPrograma.js
+++ 1-meuPrimeiroPrograma.js
@@ -1,11 +1,22 @@
 console.log("Meu Primeiro Programa: Trabalhando com variáveis");
 
+
+//variável que mostra o valor de "idade"
 const idade = 15;
 
+//mostra a variável idade
 console.log(idade);
+
+//soma a variável idade com +2
 console.log(idade+2);
+
+//subtrai a variável com -2
 console.log(idade-2);
+
+//divide a variável por 2
 console.log(idade/2);
 
+
+//variável que soma a idade com +2
 const idadeSomada = idade+2;
 console.log(idadeSomada);
